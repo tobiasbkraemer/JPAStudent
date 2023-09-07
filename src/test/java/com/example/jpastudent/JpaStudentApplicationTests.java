ea4aee6e32746ed18bce563a48c5d57ed7a2987e
tobiasbkreamer <tobias@kraemerne.dk>
2023-09-07 14:12:11 +0200

    2.0

--- src/test/java/com/example/jpastudent/JpaStudentApplicationTests.java
+++ src/test/java/com/example/jpastudent/JpaStudentApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.jpastudent;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class JpaStudentApplicationTests {
 
     @Test
